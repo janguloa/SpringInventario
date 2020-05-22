a1c715d3cce9e022777c4aec091473dfa5abad0c
janguloa <jsuesalnugo@gmail.com>
2020-05-22 14:26:01 -0500

    Implementando JWTToken

--- inventario/src/main/java/com/modelo/Usuario.java
+++ inventario/src/main/java/com/modelo/Usuario.java
@@ -64,4 +64,126 @@ public class Usuario {
 	
 	@Column(name="imagen_url")
 	private String imagen_url;
+	
+	private String token;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCod_cia() {
+		return cod_cia;
+	}
+
+	public void setCod_cia(String cod_cia) {
+		this.cod_cia = cod_cia;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+	public String getNombres() {
+		return nombres;
+	}
+
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public LocalDate getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(LocalDate fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public LocalDateTime getFecha_registro() {
+		return fecha_registro;
+	}
+
+	public void setFecha_registro(LocalDateTime fecha_registro) {
+		this.fecha_registro = fecha_registro;
+	}
+
+	public LocalDateTime getFecha_modificacion() {
+		return fecha_modificacion;
+	}
+
+	public void setFecha_modificacion(LocalDateTime fecha_modificacion) {
+		this.fecha_modificacion = fecha_modificacion;
+	}
+
+	public String getUsuario_registro() {
+		return usuario_registro;
+	}
+
+	public void setUsuario_registro(String usuario_registro) {
+		this.usuario_registro = usuario_registro;
+	}
+
+	public String getUsuario_modifico() {
+		return usuario_modifico;
+	}
+
+	public void setUsuario_modifico(String usuario_modifico) {
+		this.usuario_modifico = usuario_modifico;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+
+	public String getImagen_url() {
+		return imagen_url;
+	}
+
+	public void setImagen_url(String imagen_url) {
+		this.imagen_url = imagen_url;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
 }
